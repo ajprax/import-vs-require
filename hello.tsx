7a2d81cbbbdddd4f348be89874bae6dcc39be9a7
Aaron Feldstein <ajprax@gmail.com>
2017-07-13 15:28:46 -0700

    minimal example

---
+++ hello.tsx
@@ -0,0 +1,12 @@
+//const React = require("react");
+//const ReactDOM = require("react-dom");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+class HelloWorld extends React.Component<{}, {}> {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+ReactDOM.render(<HelloWorld/>, document.getElementById("container"));
